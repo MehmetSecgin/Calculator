cf3b8ebed305cb7125165aeff8a117433c399dd4
Mehmet Secgin <mehmetascgn@gmail.com>
2022-03-05 22:24:55 +0200

    Calculator.cs added

---
+++ ConsoleApplication1/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal static class Calculator
+    {
+        public static void Main(string[] Args)
+        {
+            var inputValid = false;
+
+            var number = 0;
+
+            string operation;
+
+            Console.WriteLine("Please input an operation. +,-,* or / are valid");
+
+            do
+            {
+                operation = Console.ReadLine()?.Trim();
+
+                if (operation == "+")
+                {
+                    Console.WriteLine("Current value is: " + number);
+                    
+                    inputValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please input a valid operation.");
+                }
+            } while (!inputValid);
+
+        }
+    }
+}
